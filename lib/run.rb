1b1973e0a01d6ce3e3135f6eee086d95ebaa3f8a
Oleh Sliusar <oleh@olehsliusar.com>
2017-02-26 19:35:10 +0200

    Add methods to class Run

--- lib/run.rb
+++ lib/run.rb
@@ -1,4 +1,18 @@
 # lib/run.rb
 class Run
+  def initialize(input)
+    @run_data = input
+  end
 
+  def duration
+    @run_data[:duration]
+  end
+
+  def distance
+    @run_data[:distance]
+  end
+
+  def timestamp
+    @run_data[:timestamp]
+  end
 end
