770d1cdb9278d931978201c0c5dcb29947b9bc3c
Oleh Sliusar <olehsliusar@gmail.com>
2017-04-12 12:42:08 +0300

    Finish that shit

--- lib/running_week.rb
+++ lib/running_week.rb
@@ -1,4 +1,18 @@
 class RunningWeek
   require_relative 'run'
-  
+  attr_reader :runs
+
+  def initialize(date, runs)
+    raise "Day not Monday" if date.wday != 1
+    @week = date.cweek
+    @runs = runs
+  end
+
+  def first_run
+    @runs.first
+  end
+
+  def average_distance
+    @runs.inject(0.0){ |sum, el| sum + el.distance }.to_f / @runs.size
+  end
 end
--- spec/running_week_spec.rb
+++ spec/running_week_spec.rb
@@ -2,34 +2,50 @@
 require 'running_week'
 
 describe RunningWeek do
+  let(:monday_run) do
+    Run.new(:duration => 32,
+            :distance => 5.2,
+            :timestamp => "2015-01-12 20:30")
+  end
 
-  describe ".count" do
+  let(:wednesday_run) do
+    Run.new(:duration => 32,
+            :distance => 5.2,
+            :timestamp => "2015-01-14 19:50")
+  end
 
-    context "with 2 logged runs this week and 1 in next" do
+  let(:runs) { [monday_run, wednesday_run] }
 
-      before(:all) do
-        2.times do
-          Run.log(:duration => rand(10),
-                  :distance => rand(8),
-                  :timestamp => "2015-01-12 20:30")
-        end
+  let(:running_week) { RunningWeek.new(Date.parse("2015-01-12"), runs) }
 
-        Run.log(:duration => rand(10),
-                :distance => rand(8),
-                :timestamp => "2015-01-19 20:30")
-      end
+  describe "#runs" do
 
-      context "without arguments" do
-        it "returns 3" do
-          expect(Run.count).to eql(3)
-        end
-      end
-
-      context "with :week set to this week" do
-        it "returns 2" do
-          expect(Run.count(:week => "2015-01-12")).to eql(2)
-        end
-      end
+    it "returns all runs in the week" do
+      expect(running_week.runs).to eql(runs)
     end
   end
+
+  describe "#first_run" do
+
+    it "returns the first run in the week" do
+      expect(running_week.first_run).to eql(monday_run)
+    end
+  end
+
+  describe "#average_distance" do
+
+    it "returns the average distance of all week's runs" do
+      expect(running_week.average_distance).to eql(5.2)
+    end
+  end
+
+  # describe "initialization" do
+  #
+  #   context "given a date which is not a Monday" do
+  #
+  #     it "raises a 'day not Monday' exception" do
+  #       expect { RunningWeek.new(Date.parse("2015-01-13"), []) }.to raise_error(RuntimeError, "Day is not Monday")
+  #     end
+  #   end
+  # end
 end
